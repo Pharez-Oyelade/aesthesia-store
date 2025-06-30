703c23ea99d650e56190e76cb49a126308a3f184
Pharez Oyelade <pharezoyelade@gmail.com>
2025-06-30 13:26:44 +0100

    productitem image transition feature rebuilt

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,8 +6,8 @@ import { IoMdHeart } from "react-icons/io";
 const ProductItem = ({ id, image, name, price, bestseller }) => {
   const { currency, addToWishlist, wishlist } = useContext(shopContext);
 
-  // const [isHovered, setIsHovered] = useState(false);
-  // const [showImage, setShowImage] = useState(image[0]);
+  const [isHovered, setIsHovered] = useState(false);
+  const [showImage, setShowImage] = useState(image[0]);
 
   const handleWishlist = (e) => {
     e.preventDefault(); // Prevent navigation when clicking heart
@@ -26,9 +26,19 @@ const ProductItem = ({ id, image, name, price, bestseller }) => {
           </span>
         )}
         <img
-          src={image[0]}
+          onMouseEnter={() => {
+            setIsHovered(true);
+            if (image[1]) setShowImage(image[1]);
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+            setShowImage(image[0]);
+          }}
+          src={showImage}
           alt=""
-          className="hover:scale-115 transition ease-in-out"
+          className={`${
+            image[1] ? "" : "hover:scale-115 transition ease-in-out"
+          }`}
         />
         <div className="absolute right-2 top-2 opacity-100 md:opacity-0 group-hover:opacity-100 transition">
           <button
