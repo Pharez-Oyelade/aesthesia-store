d2578a6f0f61f918938ddba4ac6b3268aa435c5a
Pharez Oyelade <pharezoyelade@gmail.com>
2025-07-23 18:49:10 +0100

    product controller changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -151,7 +151,10 @@ const removeProduct = async (req, res) => {
     }
 
     await productModel.findByIdAndDelete(req.body.id);
-    res.json({ success: true, message: "Product and images removed successfully" });
+    res.json({
+      success: true,
+      message: "Product and images removed successfully",
+    });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
