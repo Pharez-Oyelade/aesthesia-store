344855b859dde60b4dc37973fc81fcd6070fda96
Pharez <pharezoyelade@gmail.com>
2026-04-06 08:02:49 +0100

    minor-changes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -278,11 +278,22 @@ const Hero = () => {
         />
       </AnimatePresence>
 
+      {/* <div>
+        <motion.img
+          key={bgImage}
+          src={bgImage}
+          srcSet={srcSet}
+          sizes={sizes}
+          alt={title}
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+      </div> */}
+
       {/* Dark overlay */}
       <div className="absolute inset-0 bg-black/40 z-10" />
 
       {/* Slide Content */}
-      {/* <div className="relative z-20 flex items-center justify-center w-full h-full">
+      <div className="relative z-20 flex items-center justify-center w-full h-full">
         <AnimatePresence mode="wait">
           <motion.div
             key={current}
@@ -317,21 +328,6 @@ const Hero = () => {
             </Link>
           </motion.div>
         </AnimatePresence>
-      </div> */}
-
-      <div className="relative z-20 flex items-center justify-center w-full h-full">
-        <div className="flex flex-col items-center text-center text-white px-4">
-          <h1 className="text-3xl sm:text-5xl font-medium mb-4 drop-shadow-lg outfit-bold">
-            {title}
-          </h1>
-          <p className="text-md w-[75%] sm:w-[100%] sm:text-lg">{subtitle}</p>
-
-          <Link to={link}>
-            <button className="mt-10 px-10 py-3 rounded-full bg-gradient-to-r from-[#691110] to-pink-800 hover:from-red-900 hover:to-pink-800 text-white text-lg font-bold shadow-lg tracking-wide transition-all duration-300 border-2 border-white/20 focus:outline-none focus:ring-2 focus:ring-red-300">
-              {b_text}
-            </button>
-          </Link>
-        </div>
       </div>
 
       {/* Dots */}
