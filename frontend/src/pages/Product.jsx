ca7c5cf7aba344d0dd358abf35d81e5b39f82193
Pharez <143667740+Pharez-Oyelade@users.noreply.github.com>
2026-07-29 20:08:05 +0100

    Add 'The Haven' section to measurement requirements

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -144,7 +144,7 @@ const Product = () => {
   const requiresMeasurements =
     (productData && productData.section === "The RERE Collection") ||
     (productData && productData.section === "The Color Code Collection") ||
-    (productData && productData.section === "Unfold");
+    (productData && productData.section === "Unfold") || (productData && productData.section === "The Haven");
   const availableSizes =
     productData && productData.sizes
       ? productData.sizes.slice().sort((a, b) => Number(a) - Number(b))
